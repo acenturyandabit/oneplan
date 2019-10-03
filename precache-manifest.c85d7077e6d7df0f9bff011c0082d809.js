b9c99daad3d0c5b6db4190d8104f7253e98c1fa1
Steven Liu <steeven.liu2@gmail.com>
2019-10-03 10:51:42 +1000

    Updates

---
+++ precache-manifest.c85d7077e6d7df0f9bff011c0082d809.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0556bff3f61471df4089c1b1fa5452b8",
+    "url": "/onepage/index.html"
+  },
+  {
+    "revision": "3342bc9c15e49e21c141",
+    "url": "/onepage/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8cfd541e1c49f7cfd804",
+    "url": "/onepage/static/js/2.5e356dc6.chunk.js"
+  },
+  {
+    "revision": "3342bc9c15e49e21c141",
+    "url": "/onepage/static/js/main.ef53b8fa.chunk.js"
+  },
+  {
+    "revision": "34a8a0b440e626b9cb7e",
+    "url": "/onepage/static/js/runtime-main.c95e791a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/onepage/static/media/logo.25bf045c.svg"
+  }
+]);
